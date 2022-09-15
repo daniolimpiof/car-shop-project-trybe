95bbd282556cce8790c29f7e3894bf876357b0fc
daniolimpiof <danielly.fundcheller@gmail.com>
2022-09-15 12:31:25 -0300

    feat: :white_check_mark: Requisito 9
    Acertando a rota corretamente para readOne

--- src/Router/cars.ts
+++ src/Router/cars.ts
@@ -10,6 +10,9 @@ const carService = new CarService(car);
 const carController = new CarsController(carService);
 
 routeCar.post('/', (req, res) => carController.create(req, res));
+routeCar.get('/', (req, res) => carController.read(req, res));
 routeCar.get('/:id', (req, res) => carController.readOne(req, res));
+routeCar.put('/:id', (req, res) => carController.update(req, res));
+routeCar.delete('/:id', (req, res) => carController.delete(req, res));
 
 export default routeCar;
